0e1c85351fd6b7965a966655a05b9eb9fec6ec4e
Fyodor <dstyvsky@gmail.com>
2015-12-21 21:02:40 -0500

    sent an email

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ENV = YAML.load_file("#{RAILS_ROOT}"/config/environment_variables/)
