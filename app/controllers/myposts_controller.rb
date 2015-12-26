1e73c2c34485119ec1237dec901f39baded13f13
Fyodor <dstyvsky@gmail.com>
2015-12-26 16:40:49 -0500

    latest update

--- app/controllers/myposts_controller.rb
+++ app/controllers/myposts_controller.rb
@@ -1,2 +1,23 @@
 class MypostsController < ApplicationController
+	before_action :logged_in_user, only: [:create, :destroy]
+
+	def create
+		@mypost = current_user.myposts.build(mypost_params)
+		if @mypost.save
+			flash[:success] = "Mypost created!"
+			redirect_to root_url
+		else
+			render 'static_pages/home'
+		end
+	end
+
+	def destroy
+	end
+
+	private
+
+		def mypost_params
+			params.require(:mypost).permit(:content)
+		end
+		
 end
